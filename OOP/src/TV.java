60172e059114f28f0506e171793701a79db22782
Emma <emma.sehlhoff@hotmail.nl>
2023-04-11 16:27:22 +0200

    Kleur toegevoegd aan TV

--- OOP/src/TV.java
+++ OOP/src/TV.java
@@ -1,6 +1,7 @@
 
 public class TV extends Product{
 	public int resolutie;
+	public String kleur;
 	public String samenvatting() {
 		String samenvatting = "name = " + Name + "Price =" + Price + "electrisch?" + Electric + "waterbestendig?"
 				+ waterresistant + "de resolutie is "+ resolutie;
