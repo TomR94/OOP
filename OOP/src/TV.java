7e3dd39cc63ae08db2357fab2bdb64ad38f09a58
Tom <tomrooijmans@outlook.com>
2023-04-11 16:28:48 +0200

    second commit

--- OOP/src/TV.java
+++ OOP/src/TV.java
@@ -1,9 +1,18 @@
 
-public class TV extends Product{
-	public int resolutie;
+public class TV extends Product {
+	private int resolutie;
+
+	public int getResolutie() {
+		return resolutie;
+	}
+
+	public void setResolutie(int resolutie) {
+		this.resolutie = resolutie;
+	}
+
 	public String samenvatting() {
 		String samenvatting = "name = " + Name + "Price =" + Price + "electrisch?" + Electric + "waterbestendig?"
-				+ waterresistant + "de resolutie is "+ resolutie;
+				+ waterresistant + "de resolutie is " + resolutie;
 		return samenvatting;
-		}
+	}
 }
